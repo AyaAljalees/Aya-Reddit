f53160ee444f05a9c4ab0e2dbe7f27b9ed1844b9
aya aljalees <121544052+AyaAljalees@users.noreply.github.com>
2023-04-16 17:11:37 +0300

    add server and queries for add ,delete ,get , serach posts

---
+++ server/db/config/build.sql
@@ -0,0 +1,37 @@
+BEGIN;
+DROP TABLE IF EXISTS users,posts, comments, votes CASCADE;
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(30) NOT NULL UNIQUE,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE posts(
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(50) NOT NULL ,
+  content TEXT NOT NULL ,
+  image_url VARCHAR(50) NOT NULL ,
+  userId INTEGER NOT NULL,
+  CONSTRAINT fk_userId FOREIGN KEY (userId) REFERENCES users(id) ,
+   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP 
+ 
+);
+CREATE TABLE votes (
+    id INTEGER PRIMARY KEY ,
+    voteType TEXT NOT NULL,
+    userId INTEGER NOT NULL,
+    CONSTRAINT fk_userId FOREIGN KEY (userId) REFERENCES users(id),
+    posts_id INTEGER NOT NULL,
+    CONSTRAINT fk_postId FOREIGN KEY (posts_id) REFERENCES posts(id) 
+);
+CREATE TABLE comments (
+    id INTEGER PRIMARY KEY ,
+    content_comments TEXT NOT NULL ,
+    userId INTEGER NOT NULL,
+    CONSTRAINT fk_userId FOREIGN KEY (userId) REFERENCES users(id),
+     posts_id INTEGER NOT NULL ,
+    CONSTRAINT fk_postId FOREIGN KEY (posts_id) REFERENCES posts(id) ,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP 
+);
+COMMIT;
---
+++ server/db/queries/postSearch.js
@@ -0,0 +1,7 @@
+const { pool } = require('../config/connection');
+
+function searchPost(searchTrem) {
+  const { searchTerm } = searchTrem;
+  return pool.query('SELECT * FROM posts WHERE title ILIKE $1', [`%${searchTerm}%`]);
+}
+module.exports = { searchPost };
